4bd2eec112153e1ded21b277ed4267ab2bd8fd2f
samoletick <askar.musin@gmail.com>
2015-11-23 23:11:28 +0300

    deleted js

--- main/assets/js/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("nav01").innerHTML =
-"<ul id='menu'>" +
-"<li><a href='information.html'>Групповые игры</a></li>" +
-"<li><a href='information_playoff.html'>Плей-офф</a></li>" +
-"<li><a href='information_semifinals.html'>Полуфиналы</a></li>" +
-"<li><a href='information_finals.html'>Финалы</a></li>" +
-"</ul>";
