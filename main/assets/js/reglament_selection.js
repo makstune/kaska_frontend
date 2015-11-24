0f8463c980f67490d704d38da76a98f3387d29ed
Max <maksmantserov@gmail.com>
2015-11-24 20:30:12 +0300

    manual merging of information.html

---
+++ main/assets/js/reglament_selection.js
@@ -0,0 +1,53 @@
+$(document).ready(function () {
+		$('#btn1').show();
+        $('#btn2').hide();
+        $('#btn3').hide();
+        $('#btn4').hide();
+        $('#btn5').hide();
+        });
+		
+$(document).ready(function () {
+    $('.menubtn1').click(function () {
+        $('#btn2').hide();
+        $('#btn3').hide();
+        $('#btn4').hide();
+        $('#btn5').hide();
+        $('#btn1').show();
+    });
+});
+$(document).ready(function () {
+    $('.menubtn2').click(function () {
+        $('#btn1').hide();
+        $('#btn3').hide();
+        $('#btn4').hide();
+        $('#btn5').hide();
+        $('#btn2').show();
+    });
+});
+$(document).ready(function () {
+    $('.menubtn3').click(function () {
+        $('#btn1').hide();
+        $('#btn2').hide();
+        $('#btn4').hide();
+        $('#btn5').hide();
+        $('#btn3').show();
+    });
+});
+$(document).ready(function () {
+    $('.menubtn4').click(function () {
+        $('#btn1').hide();
+        $('#btn2').hide();
+        $('#btn3').hide();
+        $('#btn5').hide();
+        $('#btn4').show();
+    });
+});
+$(document).ready(function () {
+    $('.menubtn5').click(function () {
+        $('#btn1').hide();
+        $('#btn2').hide();
+        $('#btn3').hide();
+        $('#btn4').hide();
+        $('#btn5').show();
+    });
+});
