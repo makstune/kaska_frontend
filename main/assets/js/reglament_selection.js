c0a07e220a7026591e0b10c45e8996e7f08d209f
Max <maksmantserov@gmail.com>
2015-11-26 03:09:44 +0300

    color scheme is changed

--- main/assets/js/reglament_selection.js
+++ main/assets/js/reglament_selection.js
@@ -1,53 +1,65 @@
 $(document).ready(function () {
-		$('#btn1').show();
+		$('#btn1').show(); 
         $('#btn2').hide();
         $('#btn3').hide();
         $('#btn4').hide();
-        $('#btn5').hide();
+		
+		$("#menubtn1").addClass("sel");
         });
 		
 $(document).ready(function () {
-    $('.menubtn1').click(function () {
+    $('#menubtn1').click(function () {
         $('#btn2').hide();
         $('#btn3').hide();
         $('#btn4').hide();
-        $('#btn5').hide();
-        $('#btn1').show();
+        $('#btn1').show(); 
+		
+		$("#menubtn1").addClass("sel");
+		$("#menubtn2").removeClass("sel");
+		$("#menubtn3").removeClass("sel");
+		$("#menubtn4").removeClass("sel");
+		
     });
 });
+
 $(document).ready(function () {
-    $('.menubtn2').click(function () {
+    $('#menubtn2').click(function () {
         $('#btn1').hide();
         $('#btn3').hide();
         $('#btn4').hide();
-        $('#btn5').hide();
-        $('#btn2').show();
+        $('#btn2').show(); 
+		
+		$("#menubtn1").removeClass("sel");
+		$("#menubtn2").addClass("sel");
+		$("#menubtn3").removeClass("sel");
+		$("#menubtn4").removeClass("sel");
     });
 });
+
 $(document).ready(function () {
-    $('.menubtn3').click(function () {
+    $('#menubtn3').click(function () {
         $('#btn1').hide();
         $('#btn2').hide();
         $('#btn4').hide();
-        $('#btn5').hide();
-        $('#btn3').show();
+        $('#btn3').show(); 
+		
+		$("#menubtn1").removeClass("sel");
+		$("#menubtn2").removeClass("sel");
+		$("#menubtn3").addClass("sel");
+		$("#menubtn4").removeClass("sel");
     });
 });
+
 $(document).ready(function () {
-    $('.menubtn4').click(function () {
+    $('#menubtn4').click(function () {
         $('#btn1').hide();
         $('#btn2').hide();
         $('#btn3').hide();
-        $('#btn5').hide();
-        $('#btn4').show();
-    });
-});
-$(document).ready(function () {
-    $('.menubtn5').click(function () {
-        $('#btn1').hide();
-        $('#btn2').hide();
-        $('#btn3').hide();
-        $('#btn4').hide();
-        $('#btn5').show();
+        $('#btn4').show(); 
+		
+		$("#menubtn1").removeClass("sel");
+		$("#menubtn2").removeClass("sel");
+		$("#menubtn3").removeClass("sel");
+		$("#menubtn4").addClass("sel");
     });
 });
